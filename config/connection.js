81a1b34efe9b3cceb77d6657a645fd1d2027d0aa
Ibmug <rosv58@gmail.com>
2021-01-16 14:30:00 -0600

    Pushing to Heroku

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+
+var connection;
+
+if(ProcessingInstruction.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'secretPass',
+        database: 'burguer_db'
+    });
+}
+
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id:" + connection.threadId)
+});
+
+//Export
+module.exports = connection;
